82a5ca52efaad1a5f51b0a400d38fd3c0048fbe5
Javier Prieto <jprieton@gmail.com>
2017-09-27 16:24:25 -0430

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,82 @@
-# wp-bootstrap4-navwalker
-A custom WordPress nav walker class to fully implement the Twitter Bootstrap 4.0+ navigation style in a custom theme using the WordPress built in menu manager
+# WP Bootstrap 4.x Navwalker
+
+A custom WordPress nav walker class to fully implement the Twitter Bootstrap 4.x navigation style in a custom theme using the WordPress built in menu manager
+
+## Installation
+
+Place **wp-bootstrap-navwalker.php** in your WordPress theme folder `/wp-content/your-theme/`
+
+Open your WordPress themes **functions.php** file  `/wp-content/your-theme/functions.php` and add the following code:
+
+```php
+// Register Custom Navigation Walker
+require_once get_template_directory() . '/wp-bootstrap-navwalker.php';
+```
+
+If you encounter errors with the above code use a check like this to return clean errors to help diagnose the problem.
+
+```php
+if ( ! file_exists( get_template_directory() . '/wp-bootstrap-navwalker.php' ) ) {
+	// file does not exist... return an error.
+	return new WP_Error( 'wp-bootstrap-navwalker-missing', __( 'It appears the wp-bootstrap-navwalker.php file may be missing.', 'wp-bootstrap-navwalker' ) );
+} else {
+	// file exists... require it.
+    require_once get_template_directory . 'wp-bootstrap-navwalker.php';
+}
+```
+
+## Usage
+
+Update your `wp_nav_menu()` function in `header.php` to use the new walker by adding a "walker" item to the wp_nav_menu array.
+
+```php
+ <?php
+            wp_nav_menu( array(
+                'menu'              => 'primary',
+                'theme_location'    => 'primary',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+                'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker())
+            );
+        ?>
+``
+
+
+Typically the menu is wrapped with additional markup, here is an example of a ` navbar-fixed-top` menu that collapse for responsive navigation.
+
+```php
+<nav class="navbar navbar-default" role="navigation">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="<?php echo home_url(); ?>">
+                <?php bloginfo('name'); ?>
+            </a>
+    </div>
+
+        <?php
+            wp_nav_menu( array(
+                'menu'              => 'my-menu',
+                'theme_location'    => 'my-theme-location',
+                'depth'             => 2,
+                'container'         => 'div',
+                'container_class'   => 'collapse navbar-collapse',
+                'container_id'      => 'bs-example-navbar-collapse-1',
+                'menu_class'        => 'nav navbar-nav',
+                'fallback_cb'       => 'WP_Bootstrap_Navwalker::fallback',
+                'walker'            => new WP_Bootstrap_Navwalker())
+            );
+        ?>
+    </div>
+</nav>
+```
---
+++ wp-bootstrap-navwalker.php
@@ -0,0 +1,265 @@
+<?php
+
+/**
+ * If this file is called directly, abort.
+ */
+if ( !defined( 'ABSPATH' ) ) {
+  die( 'Direct access is forbidden.' );
+}
+
+/**
+ * WP Bootstrap Navwalker
+ *
+ * @package WP-Bootstrap-Navwalker
+ */
+/**
+ * Class Name: WP_Bootstrap_Navwalker
+ * Plugin Name: WP Bootstrap Navwalker
+ * Plugin URI:  https://github.com/jprieton/wp-bootstrap4-navwalker
+ * Description: A custom WordPress nav walker class to implement the Bootstrap 3 navigation style in a custom theme using the WordPress built in menu manager.
+ * Author: Javier Prieto
+ * Version: 2.0.5
+ * Author URI: https://github.com/jprieton/
+ * GitHub Plugin URI: https://github.com/jprieton/wp-bootstrap4-navwalker
+ * GitHub Branch: master
+ * License: GPL-3.0+
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.txt
+ */
+/* Check if Class Exists. */
+if ( !class_exists( 'WP_Bootstrap_Navwalker' ) ) {
+
+  /**
+   * WP_Bootstrap_Navwalker class.
+   *
+   * @extends Walker_Nav_Menu
+   */
+  class Bootstrap_Navwalker extends Walker_Nav_Menu {
+
+    private $dropdown = false;
+
+    public function start_lvl( &$output, $depth = 0, $args = array() ) {
+      if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+        $t = '';
+        $n = '';
+      } else {
+        $t = "\t";
+        $n = "\n";
+      }
+
+      $this->dropdown = true;
+      $output         .= $n . str_repeat( $t, $depth ) . '<div class="dropdown-menu" role="menu">' . $n;
+    }
+
+    public function end_lvl( &$output, $depth = 0, $args = array() ) {
+      if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+        $t = '';
+        $n = '';
+      } else {
+        $t = "\t";
+        $n = "\n";
+      }
+
+      $this->dropdown = false;
+      $output         .= $n . str_repeat( $t, $depth ) . '</div>' . $n;
+    }
+
+    public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+      if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+        $t = '';
+        $n = '';
+      } else {
+        $t = "\t";
+        $n = "\n";
+      }
+
+      $indent = str_repeat( $t, $depth );
+
+      $classes   = empty( $item->classes ) ? [] : (array) $item->classes;
+      $classes[] = 'menu-item-' . $item->ID;
+      $classes[] = 'nav-item';
+
+      if ( $args->walker->has_children ) {
+        $classes[] = 'dropdown';
+      }
+
+      if ( 0 < $depth ) {
+        $classes[] = 'dropdown-menu';
+      }
+
+      /**
+       * Filters the arguments for a single nav menu item.
+       *
+       * @since 1.2.0
+       *
+       * @param stdClass $args  An object of wp_nav_menu() arguments.
+       * @param WP_Post  $item  Menu item data object.
+       * @param int      $depth Depth of menu item. Used for padding.
+       */
+      $args = apply_filters( 'nav_menu_item_args', $args, $item, $depth );
+
+      /**
+       * Filters the CSS class(es) applied to a menu item's list item element.
+       *
+       * @since 3.0.0
+       * @since 4.1.0 The `$depth` parameter was added.
+       *
+       * @param array    $classes The CSS classes that are applied to the menu item's `<li>` element.
+       * @param WP_Post  $item    The current menu item.
+       * @param stdClass $args    An object of wp_nav_menu() arguments.
+       * @param int      $depth   Depth of menu item. Used for padding.
+       */
+      $class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args, $depth ) );
+      $class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+      /**
+       * Filters the ID applied to a menu item's list item element.
+       *
+       * @since 3.0.1
+       * @since 4.1.0 The `$depth` parameter was added.
+       *
+       * @param string   $menu_id The ID that is applied to the menu item's `<li>` element.
+       * @param WP_Post  $item    The current menu item.
+       * @param stdClass $args    An object of wp_nav_menu() arguments.
+       * @param int      $depth   Depth of menu item. Used for padding.
+       */
+      $id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args, $depth );
+      $id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+      if ( !$this->dropdown ) {
+        $output .= $indent . '<li' . $id . $class_names . '>' . $n . $indent . $t;
+      }
+
+      $atts           = array();
+      $atts['title']  = !empty( $item->attr_title ) ? $item->attr_title : '';
+      $atts['target'] = !empty( $item->target ) ? $item->target : '';
+      $atts['rel']    = !empty( $item->xfn ) ? $item->xfn : '';
+      $atts['href']   = !empty( $item->url ) ? $item->url : '';
+
+      /**
+       * Filters the HTML attributes applied to a menu item's anchor element.
+       *
+       * @since 3.6.0
+       * @since 4.1.0 The `$depth` parameter was added.
+       *
+       * @param array $atts {
+       *     The HTML attributes applied to the menu item's `<a>` element, empty strings are ignored.
+       *
+       *     @type string $title  Title attribute.
+       *     @type string $target Target attribute.
+       *     @type string $rel    The rel attribute.
+       *     @type string $href   The href attribute.
+       * }
+       * @param WP_Post  $item  The current menu item.
+       * @param stdClass $args  An object of wp_nav_menu() arguments.
+       * @param int      $depth Depth of menu item. Used for padding.
+       */
+      $atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args, $depth );
+
+      if ( $args->walker->has_children ) {
+        $atts['data-toggle']   = 'dropdown';
+        $atts['aria-haspopup'] = 'true';
+        $atts['aria-expanded'] = 'false';
+      }
+
+      $attributes = '';
+      foreach ( $atts as $attr => $value ) {
+        if ( !empty( $value ) ) {
+          $value      = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+          $attributes .= ' ' . $attr . '="' . $value . '"';
+        }
+      }
+
+      /** This filter is documented in wp-includes/post-template.php */
+      $title = apply_filters( 'the_title', $item->title, $item->ID );
+
+      /**
+       * Filters a menu item's title.
+       *
+       * @since 4.4.0
+       *
+       * @param string   $title The menu item's title.
+       * @param WP_Post  $item  The current menu item.
+       * @param stdClass $args  An object of wp_nav_menu() arguments.
+       * @param int      $depth Depth of menu item. Used for padding.
+       */
+      $title = apply_filters( 'nav_menu_item_title', $title, $item, $args, $depth );
+
+      $item_classes = [ 'nav-link' ];
+
+      if ( $args->walker->has_children ) {
+        $item_classes[] = 'dropdown-toggle';
+      }
+
+      if ( 0 < $depth ) {
+        $item_classes[] = 'dropdown-item';
+      }
+
+      $item_output = $args->before;
+      $item_output .= '<a class="' . implode( ' ', $item_classes ) . '" ' . $attributes . '>';
+      $item_output .= $args->link_before . $title . $args->link_after;
+      $item_output .= '</a>';
+      $item_output .= $args->after;
+
+      /**
+       * Filters a menu item's starting output.
+       *
+       * The menu item's starting output only includes `$args->before`, the opening `<a>`,
+       * the menu item's title, the closing `</a>`, and `$args->after`. Currently, there is
+       * no filter for modifying the opening and closing `<li>` for a menu item.
+       *
+       * @since 3.0.0
+       *
+       * @param string   $item_output The menu item's starting HTML output.
+       * @param WP_Post  $item        Menu item data object.
+       * @param int      $depth       Depth of menu item. Used for padding.
+       * @param stdClass $args        An object of wp_nav_menu() arguments.
+       */
+      $output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+    }
+
+    public function end_el( &$output, $item, $depth = 0, $args = array() ) {
+      if ( isset( $args->item_spacing ) && 'discard' === $args->item_spacing ) {
+        $t = '';
+        $n = '';
+      } else {
+        $t = "\t";
+        $n = "\n";
+      }
+
+      $output .= $this->dropdown ? '' : str_repeat( $t, $depth ) . '</li>' . $n;
+    }
+
+    /**
+     * Menu Fallback
+     *
+     * @since 1.2.0
+     *
+     * @param array $args passed from the wp_nav_menu function.
+     */
+    public static function fallback( $args ) {
+      if ( current_user_can( 'edit_theme_options' ) ) {
+
+        $defaults = [
+            'container'       => 'div',
+            'container_id'    => false,
+            'container_class' => false,
+            'menu_class'      => 'menu',
+            'menu_id'         => false,
+        ];
+        $args     = wp_parse_args( $args, $defaults );
+        if ( !empty( $args['container'] ) ) {
+          echo sprintf( '<%s id="%s" class="%s">', $args['container'], $args['container_id'], $args['container_class'] );
+        }
+        echo sprintf( '<ul id="%s" class="%s">', $args['container_id'], $args['container_class'] ) .
+        '<li class="nav-item">' .
+        '<a href="' . admin_url( 'nav-menus.php' ) . '" class="nav-link">' . __( 'Add a menu', \SourceFramework\TEXTDOMAIN ) . '</a>' .
+        '</li></ul>';
+        if ( !empty( $args['container'] ) ) {
+          echo sprintf( '</%s>', $args['container'] );
+        }
+      }
+    }
+
+  }
+
+}
